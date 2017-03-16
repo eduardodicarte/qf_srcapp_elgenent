50d328a41d982d439607411438528514f19d7afa
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2017-03-16 00:58:20 -0300

    Criação da classe responsável pela recuperação dos dados no banco

--- db/dbconnect.rb
+++ db/dbconnect.rb
@@ -1,15 +1,44 @@
 require "pg"
 
 class DB
-  #def initialize(ip, port, dbname, dbuser, passwd)
-    #connect(ip, port, dbname, dbuser, passwd)
-  #end
+  def initialize(ip, port, dbname, dbuser, passwd)
+    @connection = PG::Connection.new(ip, port, nil, nil, dbname, dbuser, passwd)
+  end
 
-  def connect(ip, port, dbname, dbuser, passwd)
-    @conn = PG::Connection.new(ip, port, nil, nil, dbname, dbuser, passwd)
+  def selectData(table, fields, params, values)
+    fieldsStr = (fields != nil && fields.length > 0) ? getFields(fields) : " * "
+    whereStr  = (params != nil && params.length > 0) ? "WHERE ".concat(params[0]).concat("=").concat(values[0]) : ""
+    andStr    = (params != nil && params.length > 1) ? getParamsAnd(params, values)  : ""
+
+    sql = "SELECT #{fieldsStr} FROM #{table} #{whereStr} #{andStr}"
+    puts sql
+
+    return @connection.exec(sql)
+  end
+
+  def getParamsAnd(params, values)
+    andStr = ""
+
+    for param, val in [params, values]
+      andStr.concat(param.concat("=").concat(val))
+    end
+
+    return andStr
+  end
+
+  def getFields(fields)
+    ret = ""
+
+    for field in fields
+      ret.concat(",") if ret.length > 0
+
+      ret.concat(field)
+    end
+
+    return ret
   end
 
   def close
-    @conn.close if @conn
+    @connection.close if @connection
   end
 end
--- main.rb
+++ main.rb
@@ -8,6 +8,22 @@ passwd = "AvdWe&sd#"
 
 begin
   puts "Tentando connectar ".concat(ip).concat(":").concat(port)
+  db = DB.new(ip, port, dbname, dbuser, passwd)
+
+  params = Array.new
+  params.push("table_schema")
+
+  values = Array.new
+  values.push("'el'")
+
+  table = "information_schema.tables"
+
+  fields = Array.new
+  fields.push("table_name")
+
+  tables = db.selectData(table, fields, params, values)
+
+  db.close
 rescue Exception => e
   puts e
 end
